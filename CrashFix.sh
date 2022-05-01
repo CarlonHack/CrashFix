461215c15582cd6d6ad5f69fc02b4def58b2ff75
CARLON <99050212+CarlonHack@users.noreply.github.com>
2022-05-01 18:37:03 +0300

    Add files via upload

---
+++ CrashFix.sh
@@ -0,0 +1,25 @@
+clear
+pkg install cmatrix
+clear
+date
+echo ""
+sleep 3
+echo -n "Device        : "
+getprop ro.product.brand
+sleep 2
+echo -n "Device Type   : "
+getprop ro.product.model
+sleep 3
+echo -e "                                           \033[31mCrash Fix\033[0m"             
+echo ""
+sleep 3
+echo -e "\033[32mOyundan Atma Onarılıyor...\033[0m"
+echo ""
+sleep 5
+echo -e "\033[32mLog Temizleniyor...\033[0m"
+sleep 5
+echo ""
+echo -e "\033[32mOyundan Atma Onarıldı\033[0m"
+echo ""
+"
+ Carlon Hack\033[0m"3[0m""\033[0m"0m"0m"[0m"
